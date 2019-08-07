9c3b09563fc4a994cde8268fe1c5870c56538036
Budbreak <liqilog@163.com>
2019-08-07 18:05:47 +0800

    'finshed'

---
+++ src/main/java/com/mmall/util/DateTimeUtil.java
@@ -0,0 +1,29 @@
+package com.mmall.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.Date;
+
+public class DateTimeUtil {
+    //joda-time jar包
+    public static final String STANDARD_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    //str -> Date
+    public static Date strToDate(String dateTimeStr) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern(STANDARD_FORMAT);
+        DateTime dateTime = dateTimeFormatter.parseDateTime(dateTimeStr);
+        return dateTime.toDate();
+    }
+
+    //Date -> str
+    public static String dateToStr(Date date) {
+        if (date == null) {
+            return StringUtils.EMPTY;
+        }
+        DateTime dateTime = new DateTime(date);
+        return dateTime.toString(STANDARD_FORMAT);
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,20 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+
+<p>springmvc上传文件</p>
+<form name="from1" action="/manage/product/upload.do" method="post" enctype="multipart/form-data">
+    <input type="file" name="upload_file" />
+    <input type="submit" name="springmvc上传文件" />
+
+</form>
+
+<p>富文本图片上传文件</p>
+<form name="from2" action="/manage/product/richtext_img_upload.do" method="post" enctype="multipart/form-data">
+    <input type="file" name="upload_file" />
+    <input type="submit" name="富文本图片上传文件" />
+
+</form>
+
 </body>
 </html>
